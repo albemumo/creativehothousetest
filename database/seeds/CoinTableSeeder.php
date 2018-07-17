cbb728d8caf9d0d3afd614402545eea061421792
albemumo <alberto.munoz@freaksheep.com>
2018-07-17 15:22:59 +0200

    Improving seeder console messages

--- database/seeds/CoinTableSeeder.php
+++ database/seeds/CoinTableSeeder.php
@@ -11,28 +11,29 @@ class CoinTableSeeder extends Seeder
 
     /**
      * Run the database seeds.
-     *
      * @return void
+     * @throws Exception
      */
     public function run()
     {
-        Coin::truncate();
-
         try {
             $this->client = new Client(['base_uri' => 'https://api.coinmarketcap.com/v2/ticker/']);
 
             $start = 1;
             $limit = 1;
             $this->getCurrencies($start, $limit);
+            $this->command->info($this->totalCryptocurrencies . ' Coins founded in CoinMarketCap API.');
+            $this->command->info('Coin database insert status:');
 
             $start = 2;
             $limit = 50;
             do {
+                $this->command->comment($start . ' of ' . $this->totalCryptocurrencies . ' Coins...');
                 $this->getCurrencies($start, $limit);
 
                 $start += 50;
             } while ($start < $this->totalCryptocurrencies);
-
+            $this->command->info('Coins table seeding finished!');
         } catch (Exception $e) {
             throw new Exception($e->getMessage(), $e->getCode());
         }
@@ -43,7 +44,7 @@ class CoinTableSeeder extends Seeder
      * @param $limit
      * @throws Exception
      */
-    private function getCurrencies($start, $limit) {
+    private function getCurrencies(int $start = 1, int $limit = 1) {
         $response = $this->client->get('?convert=BTC&start=' . $start . '&limit=' . $limit . '&sort=id');
         $responseBody = $response->getBody();
         $responseArray = json_decode($responseBody, true);
@@ -59,7 +60,10 @@ class CoinTableSeeder extends Seeder
         $this->createCoin($responseDataArray);
     }
 
-    private function createCoin($currencies) {
+    /**
+     * @param $currencies
+     */
+    private function createCoin(array $currencies = null) {
         foreach($currencies as $item) {
             Coin::create([
                 'name' => $item['name'],
