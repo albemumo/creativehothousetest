e33e26112221a1fecb96e78414841734cccb7e9d
albemumo <alberto.munoz@freaksheep.com>
2018-07-19 14:53:26 +0200

    Fixing coins_historicals table fields. Creating CoinsTableSeeder

--- database/migrations/2018_07_16_182918_create_coins_historicals_table.php
+++ database/migrations/2018_07_16_182918_create_coins_historicals_table.php
@@ -17,7 +17,7 @@ class CreateCoinsHistoricalsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('coin_id');
             $table->foreign('coin_id')->references('id')->on('coins');
-            $table->decimal('price_usd', 16, 9);
+            $table->decimal('price_usd', 16, 9)->nullable();
             $table->dateTimeTz('snapshot_at');
         });
     }
