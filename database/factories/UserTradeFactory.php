d7666d827987a55a79b602395f2b59efe9d616c3
albemumo <alberto.munoz@freaksheep.com>
2018-07-19 14:56:50 +0200

    Creating UserTradeFactory

---
+++ database/factories/UserTradeFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\UserTrade::class, function (Faker $faker) {
+    return [
+        'coin_id' => function () {
+            return factory(App\Coin::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'amount' => $faker->randomFloat(2, 0, 9999999999),
+        'price_usd' => $faker->randomFloat(2, 0, 9999999999),
+        'notes' => $faker->text,
+        'traded_at' => $faker->dateTime(),
+    ];
+});
