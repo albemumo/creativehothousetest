a05e16cec8d1598a8ebd4da239838bc4a56fd7bd
albemumo <alberto.munoz@freaksheep.com>
2018-07-19 14:55:25 +0200

    Creating CoinHistorical Factory

--- database/factories/CoinHistoricalFactory.php
+++ database/factories/CoinHistoricalFactory.php
@@ -1,9 +1,14 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(App\CoinHistorical::class, function (Faker $faker) {
     return [
-        //
+        'coin_id' => function () {
+            return factory(App\Coin::class)->create()->id;
+        },
+        'price_usd' => $faker->randomFloat(9),
+        'snapshot_at' => $faker->dateTime('now')->format('Y-m-d H:i:s'),
     ];
 });
