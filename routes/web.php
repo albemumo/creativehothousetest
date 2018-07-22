df35a9030038350d80c33bfc5b21811bea063801
albemumo <alberto.munoz@freaksheep.com>
2018-07-22 16:33:37 +0200

    Adding VUE client to login and register.

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/home', 'HomeController@index')->name('home');
+
+//Auth::routes();
+//Route::get('/home', 'HomeController@index')->name('home');
